5c19344bd75c378dc2e850f406c23e71f9101324
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2024-01-31 20:46:33 +0000

    created the foundation for the board. NEXT ADD ALL THE STONES IN THE RIGHT PLACES

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <vector>
 
 int main () {
-  
+  std::vector<std::vector<int>> Board {std::vector<int>(8), std::vector<int>(8)};
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 8; j++) {
+        std::cout << Board.at(i).at(j);
+    }
+    std::cout << '\n';
+  }
 }
