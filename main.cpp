a4c38fe3a6482367af0855bb826bab9bb23f52d4
jlinares12 <142842452+jlinares12@users.noreply.github.com>
2024-02-01 07:30:23 +0000

    made the board verticle and set up the board so that every playable spot starts off with 4 stones. I also created the skeleton for class Player

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <vector>
 
+class Player {
+  public:
+
+  private:
+     
+};
+
 int main () {
-  std::vector<std::vector<int>> Board {std::vector<int>(8), std::vector<int>(8)};
-  for (int i = 0; i < 2; i++) {
-    for (int j = 0; j < 8; j++) {
-        std::cout << Board.at(i).at(j);
+  std::vector<std::vector<int>> Board (8, std::vector<int>(2));
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 2; j++) {
+      if (i == 0 || i == 7) {
+        continue;
+      }
+      Board.at(i).at(j) = 4;
+    }
+  }
+  
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 2; j++) {
+      std::cout<<Board.at(i).at(j);
     }
     std::cout << '\n';
   }
