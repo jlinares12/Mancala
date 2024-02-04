4593f343b6b924224b23db2bbbaaeb5c7aa6702d
Juan Linares <juanlinares821@csu.fullerton.edu>
2024-02-03 21:40:32 -0800

    created a function that prints out the board in the perspective of player 2

--- main.cpp
+++ main.cpp
@@ -3,14 +3,24 @@
 
 class Player {
   public:
-
+    
   private:
       
 };
 
-void PrintBoard(std::vector<std::vector<int>>& Board) {
+void PrintPlayer1 (const std::vector<std::vector<int>>& Board) {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 2; j++) {
+      std::cout<<Board.at(i).at(j);
+    }
+    std::cout << '\n';
+  }
+   std::cout << "=======\n";
+}
+
+void PrintPlayer2 (const std::vector<std::vector<int>>& Board) {
+  for (int i = 7; i >= 0; i--) {
+    for (int j = 1; j >= 0; j--) {
       std::cout<<Board.at(i).at(j);
     }
     std::cout << '\n';
@@ -43,7 +53,7 @@ void MovePlayer2 (int i_start, std::vector<std::vector<int>>& Board) {
   int amount_stones {Board.at(i_start).at(1)};                                //sets up the limit of stones you can place on the board = #stones in pocket
   Board.at(i_start).at(1) = 0;                                                //sets pocket played to 0
   
-  for (int i = i_start - 1; i >= 0; i--) {                                     //for loop runs down left side of board adding one to each pocket, starts on pocket chosen to play
+  for (int i = i_start - 1; i >= 0; i--) {                                     //for loop runs up right side of board adding one to each pocket, starts on pocket chosen to play
     while (amount_stones > 0){                                                //while loop checks to see if we still have stones left
       Board.at(i).at(1) += 1;
       amount_stones--;
@@ -51,7 +61,7 @@ void MovePlayer2 (int i_start, std::vector<std::vector<int>>& Board) {
     }
   }
 
-  for (int i = 1; i < 8; i++) {                                               //for loop runs up right side of the board adding one to each pocket
+  for (int i = 1; i < 8; i++) {                                               //for loop runs down left side of the board adding one to each pocket
     while(amount_stones > 0) {                                                //while loop serves same porpuse as previous one
       Board.at(i).at(0) += 1;
       amount_stones--;
@@ -71,16 +81,18 @@ int main () {
     }
   }
   
-  PrintBoard(Board);
+  PrintPlayer1(Board);
 
   int hole{0};
   std::cout << "Please enter number of pocket to play: ";
   std::cin >> hole;
   MovePlayer1(hole, Board);
 
-  PrintBoard(Board);
+  PrintPlayer1(Board);
+  std::cout << "Player 2's turn\n";
+  PrintPlayer2(Board);
   std::cout << "Please enter number of pocket to play: ";
   std::cin >> hole;
   MovePlayer2 (hole, Board);
-  PrintBoard(Board);
+  PrintPlayer2(Board);
 }
