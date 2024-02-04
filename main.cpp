35f7d3185642ac1495f3ec78701e13523f8b9bc3
Juan Linares <juanlinares821@csu.fullerton.edu>
2024-02-03 21:32:02 -0800

    I created function MovePlayer2 that is in charge of the mechanics for player 2

--- main.cpp
+++ main.cpp
@@ -39,6 +39,27 @@ void MovePlayer1 (int i_start, std::vector<std::vector<int>>& Board) {        //
   }
 }
 
+void MovePlayer2 (int i_start, std::vector<std::vector<int>>& Board) {
+  int amount_stones {Board.at(i_start).at(1)};                                //sets up the limit of stones you can place on the board = #stones in pocket
+  Board.at(i_start).at(1) = 0;                                                //sets pocket played to 0
+  
+  for (int i = i_start - 1; i >= 0; i--) {                                     //for loop runs down left side of board adding one to each pocket, starts on pocket chosen to play
+    while (amount_stones > 0){                                                //while loop checks to see if we still have stones left
+      Board.at(i).at(1) += 1;
+      amount_stones--;
+      break;                                                                  //break while loop to move to the next pocket before checking condition again
+    }
+  }
+
+  for (int i = 1; i < 8; i++) {                                               //for loop runs up right side of the board adding one to each pocket
+    while(amount_stones > 0) {                                                //while loop serves same porpuse as previous one
+      Board.at(i).at(0) += 1;
+      amount_stones--;
+      break;
+    }
+  }
+}
+
 int main () {
   std::vector<std::vector<int>> Board (8, std::vector<int>(2));               //sets up blank board
   for (int i = 0; i < 8; i++) {                                               //start of game sets board up with 4 stones in each playable pocket
@@ -57,5 +78,9 @@ int main () {
   std::cin >> hole;
   MovePlayer1(hole, Board);
 
+  PrintBoard(Board);
+  std::cout << "Please enter number of pocket to play: ";
+  std::cin >> hole;
+  MovePlayer2 (hole, Board);
   PrintBoard(Board);
 }
