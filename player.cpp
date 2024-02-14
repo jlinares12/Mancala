7a0a548837c03596ae8f29fedc52e545c9eb1b5c
Juan Linares <juanlinares821@csu.fullerton.edu>
2024-02-14 12:33:14 -0800

    added the feature where you can steal stones from the opposite pocket if the last stone you land on is empty. Currently the feature is only available for player 1

--- player.cpp
+++ player.cpp
@@ -9,12 +9,22 @@ Player1::Player1 (bool turn, int points) :  m_turn (turn), m_points (points) {}
 void Player1::Move (int start, std::vector<std::vector<int>>& Board) {        //takes in which pocket the player chooses to play & current state of board
       int amount_stones {Board.at(start).at(0)};                              //sets up the limit of stones you can place on the board = #stones in pocket
       int stone = amount_stones;
+      int stolen_stones {0};
       Board.at(start).at(0) = 0;                                                //sets pocket played to 0
   
     for (int i = start + 1; i < 8; i++) {                                     //for loop runs down left side of board adding one to each pocket, starts on pocket chosen to play
         while (amount_stones > 0){                                                //while loop checks to see if we still have stones left
           Board.at(i).at(0) += 1;
           amount_stones--;
+          if (amount_stones == 0) {
+            if (Board.at(i).at(0) == 1) {
+                stolen_stones = Board.at(i).at(1);
+                Board.at(i).at(1) = 0;
+
+                Board.at(7).at(0) += stolen_stones;
+                stolen_stones = 0;
+            }
+          }
           break;                                                                  //break while loop to move to the next pocket before checking condition again
         }
     }
@@ -23,6 +33,15 @@ void Player1::Move (int start, std::vector<std::vector<int>>& Board) {        //
         while(amount_stones > 0) {                                                //while loop serves same porpuse as previous one
           Board.at(i).at(1) += 1;
           amount_stones--;
+          if (amount_stones == 0) {
+            if (Board.at(i).at(1) == 1) {
+                stolen_stones = Board.at(i).at(0);
+                Board.at(i).at(0) = 0;
+
+                Board.at(7).at(0) += stolen_stones;
+                stolen_stones = 0;
+            }
+          }
           break;
         }
     }
